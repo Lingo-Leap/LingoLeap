977f997606946fba789f74ffcf050b7f85d759c4
chakeradel <chakeradel30@gmail.com>
2024-09-17 15:46:02 +0100

    database already done

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 // const cors = require("cors");
 
 require("dotenv").config();
-require("./config");
+require("./config/config");
 require("./models")
 
 const app = express();
---
+++ backend/models/languageUsers.model.js
@@ -0,0 +1,8 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/config'); 
+
+const LanguageUsers = sequelize.define('LanguageUsers', {
+
+});
+
+module.exports = LanguageUsers;
