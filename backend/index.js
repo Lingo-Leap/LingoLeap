eaae04ddc9b94f5c38d80f9bca706d0609d69b73
AHMD-GTAT <hameda.ahmed2929@gmail.com>
2024-09-17 08:20:13 +0100

    running server & front

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const cors = require("cors");
+
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.listen(port, () => {
+    console.log(`Server running on port  http://localhost:${port}`);
+  });
