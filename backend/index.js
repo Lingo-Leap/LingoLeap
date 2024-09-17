ea5049364756a3354b875d4a855156d52ac2d279
AHMD-GTAT <hameda.ahmed2929@gmail.com>
2024-09-17 08:42:07 +0100

    added new files

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,15 @@
 const express = require("express");
-const cors = require("cors");
+// const cors = require("cors");
 
 require("dotenv").config();
+require("./config");
+require("./models")
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+// app.use(cors())
 
 app.listen(port, () => {
     console.log(`Server running on port  http://localhost:${port}`);
